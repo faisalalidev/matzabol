8381d062d59b8b4f16f74e4fd2abbb5cc81fe1f8
Faisal <106175838+NomaanBudhwani@users.noreply.github.com>
2023-05-04 11:29:34 -0500

    Matched user fixes

--- app/Http/Controllers/Api/EventAPIController.php
+++ app/Http/Controllers/Api/EventAPIController.php
@@ -46,17 +46,22 @@ class EventAPIController extends Controller
         if($request->user_id){
             $user = User::where('id',$request->user_id)->first();
             // Get all the other users at the event that the current user hasn't already matched with
-            $otherUsers = $event->users()->whereNotIn('users.id', function ($query) use ($request, $event) {
+            $otherUsers = $event->users()->whereNotIn('users.id', function ($query) use ($user, $event) {
                 $query->select('matched_id')
                     ->from('event_user_matches')
                     ->where('event_id', $event->id)
-                    ->where(function ($q) use ($request) {
-                        $q->where('user_id', $request->user_id)
-                            ->orWhere('matched_id', $request->user_id);
+                    ->where(function ($q) use ($user) {
+                        $q->where('user_id', $user->id)
+                            ->orWhere('matched_id', $user->id);
                     });
-            })->where('users.id', '<>', $request->user_id) // Exclude the current user
+            })
+            ->whereNotIn('users.id', function ($query) use ($user) {
+                $query->select('user_id')
+                    ->from('event_user_matches')
+                    ->where('matched_id', $user->id);
+            })
+            ->where('users.id', '!=', $user->id)
             ->get();
-
             // Get the IDs of the users that the current user has already matched with
             $alreadyMatchedIds = $user ? $user->matches()->pluck('matched_id')->toArray() : [];
             // Filter out any users that the current user has already matched with
@@ -67,26 +72,19 @@ class EventAPIController extends Controller
             if ($possibleMatches->isEmpty()) {
                 return RESTAPIHelper::response(['error' => 'No possible matches'], 404, 'No possible matches.', $this->isBlocked);
             }
-
-
-//            dd($possibleMatches);
-            // Pick a random user to match with
             $matchedUser = $possibleMatches->random();
-
             // Create the match record
             $match = new EventUserMatch();
             $match->event_id = $event->id;
             $match->user_id = $request->user_id;
             $match->matched_id = $matchedUser->id;
             $match->save();
-
-
-
-//            return response()->json([
-//                'message' => 'Matched with user successfully',
-//                'match' => $match,
-//            ]);
-            return RESTAPIHelper::response(['match' => $match], 200, 'Matched with user successfully.', $this->isBlocked);
+            $match->load(['user', 'matchedUser']);
+            return RESTAPIHelper::response([
+//                'user' => $match->user,
+                'matchedUser' => $match->matchedUser,
+            ],
+                200, 'Matched with user successfully.', $this->isBlocked);
         }
 
         return RESTAPIHelper::response(['event' => $event], 200, 'Event fetch successfully.', $this->isBlocked);
--- app/Models/EventUserMatch.php
+++ app/Models/EventUserMatch.php
@@ -15,4 +15,17 @@ class EventUserMatch extends Model
         'event_id',
         'matched_id',
     ];
+
+    protected $with = [
+        'users'
+    ];
+    public function user ()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function matchedUser()
+    {
+        return $this->belongsTo(User::class,'matched_id');
+    }
 }
