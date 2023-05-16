044032a7cef380fe9c300ab8c40f08d74590a5c3
Faisal <105174254+faisalalidev@users.noreply.github.com>
2023-05-16 18:17:55 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -85,7 +85,7 @@ Route::post('/incoming-call', function (\Illuminate\Http\Request $request) {
     // Perform any necessary logic, such as validating the caller or initiating the call
     $response = new \Twilio\TwiML\VoiceResponse();
     $dial = $response->dial();
-    $dial->client('1098');
+    $dial->client('1096');
     return $response;
 });
 
